61c37faf5d3582235a5a61b67ac3d2c14d2bcda4
suswaranrachakonda-coder <suswaranrachakonda@gmail.com>
2025-12-11 15:44:51 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,15 @@
+public class calculator {
+    public static void main(String[] args) {
+       int n1 =20;
+         int n2 =10;
+        int w = n1+n2;
+        int x = n1-n2;
+        int y = n1*n2;
+        int z= n1/n2;
+        System.out.println("The add is: " + w);
+        System.out.println("The sub is: " + x);
+        System.out.println("The multi is: " + y);
+        System.out.println("The div is: " + z);
+        
+    }
+}
