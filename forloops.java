e10708c8c90ebd8f6e68c735f1ad9302895ba3eb
suswaranrachakonda-coder <suswaranrachakonda@gmail.com>
2025-12-11 11:18:30 +0530

    Add files via upload

---
+++ forloops.java
@@ -0,0 +1,11 @@
+public class forloops {
+    public static void main(String[] args) {
+        for (int x=5; x>-1; x--) {
+            for (int j=-1;j<x;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
