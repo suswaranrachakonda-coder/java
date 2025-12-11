2f244d91dfdcd8268a484262c72784de6f6d10c9
suswaranrachakonda-coder <suswaranrachakonda@gmail.com>
2025-12-11 11:39:01 +0530

    Add files via upload

---
+++ functions.java
@@ -0,0 +1,11 @@
+public class functions {
+     
+    public static void main(String[] args) {
+        System.out.println(add(2,3));
+    }
+    public static int add(int a , int b) {
+        return a+b;
+    }
+        
+    
+}
