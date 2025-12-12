977a5a1620e87eaed83eda1f8593ccf1560b15c8
suswaranrachakonda-coder <suswaranrachakonda@gmail.com>
2025-12-12 09:24:29 +0530

    Add files via upload

---
+++ BasicTryCatch.java
@@ -0,0 +1,13 @@
+public class BasicTryCatch {
+    public static void main(String[] args) {
+        try {
+            int a = 10;     // sample values
+            int b = 0;      // this will cause ArithmeticException
+            int result = a / b;
+
+            System.out.println("Result is: " + result);
+        } catch (Exception e) {
+            System.out.println("Caught Exception: " + e.getMessage());
+        }
+    }
+}
